cb3070ba7b2207977eb1d029efa1521b59195fca
Kim Tiago Baptista <kimtiago@gmail.com>
2023-11-05 16:03:13 +0000

    adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Porque Deus amou ao mundo de tal maneira que deu o seu Filho unigênito, para que todo o que nele crê não pereça, mas tenha a vida eterna. João 3:16</h1>
+}
+
+export default Home;
