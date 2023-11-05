560070dee71a8c6c6287db0019fcd766cc1d0202
Kim Tiago Baptista <kimtiago@gmail.com>
2023-11-05 21:41:24 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function Home() {
-  return <h1>Porque Deus amou ao mundo de tal maneira que deu o seu Filho unigênito, para que todo o que nele crê não pereça, mas tenha a vida eterna. João 3:16</h1>
+  return (
+    <h1>
+      Porque Deus amou ao mundo de tal maneira que deu o seu Filho unigênito,
+      para que todo o que nele crê não pereça, mas tenha a vida eterna. João
+      3:16
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
