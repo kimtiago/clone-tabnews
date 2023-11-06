f8db9e521dfabcb61599b48649b6540513af6029
Kim Tiago Baptista <kimtiago@gmail.com>
2023-11-06 21:02:25 +0000

    add `database.js` and `.env` files

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = await new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
