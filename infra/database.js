eb4c4b898bf59470fa29eda5d4d2e83a7ef936c3
Kim Tiago Baptista <kimtiago@gmail.com>
2024-03-12 15:27:38 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
   try {
