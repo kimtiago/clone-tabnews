d7da77c0bfd696d15be64f5301754f0fcedc3fb4
Kim Tiago Baptista <kimtiago@gmail.com>
2026-04-14 16:36:18 +0000

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -32,6 +32,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "kimtiago",
         email: "kimtiago@email.com",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
