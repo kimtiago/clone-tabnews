e2180d86d3ecb5a8d82aa63245b521e8f2e10a7e
Kim Tiago Baptista <kimtiago@gmail.com>
2026-04-27 20:25:57 +0000

    feat: add default feature `read:activation_token` when creating `user`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -32,7 +32,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "kimtiago",
         email: "kimtiago@email.com",
-        features: [],
+        features: ["read:activation_token"],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
